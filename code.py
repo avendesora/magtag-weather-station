b4b6178e45b4ed2d60ffa099754b2bcc3b463c7c
Nathan Patton <npatton@gmail.com>
2021-10-04 13:47:00 -0500

    initial commit

--- code.py
+++ code.py
@@ -1,42 +1,49 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
-#
-# SPDX-License-Identifier: Unlicense
 import time
 from adafruit_magtag.magtag import MagTag
 
 magtag = MagTag()
 
-# magtag.add_text(
-#    text_position=(
-#        50,
-#        (magtag.graphics.display.height // 2) - 1,
-#    ),
-#    text_scale=3,
-# )
+magtag.add_text(
+    text_position=(
+        50,
+        (magtag.graphics.display.height // 2) - 1,
+    ),
 
-# magtag.set_text("Hello World")
+    text_scale=3,
+)
 
-# button_colors = ((255, 0, 0), (255, 150, 0), (0, 255, 255), (180, 0, 255))
-# button_tones = (1047, 1318, 1568, 2093)
+magtag.set_text("")
 
 while True:
-    for i, b in enumerate(magtag.peripherals.buttons):
-        if not b.value:
-            if i == 0:
-                print("Hi!")
-            elif i == 1:
-                print("Blah!")
-            elif i == 2:
-                print("Wowzers!")
-            elif i == 3:
-                print("Bye!")
+    for index, button in enumerate(magtag.peripherals.buttons):
+        if not button.value:
+            # Display Digital Time
+            if index == 0:
+                magtag.peripherals.neopixel_disable = False
+                magtag.peripherals.neopixels.fill((255, 255, 255))
+                magtag.set_text("Howdy!")
 
-            # magtag.peripherals.neopixel_disable = False
-            # magtag.peripherals.neopixels.fill(button_colors[i])
+            # Display Inside Temperature and Gas Levels
+            elif index == 1:
+                magtag.peripherals.neopixel_disable = False
+                magtag.peripherals.neopixels.fill((255, 0, 0))
+                magtag.set_text("")
+
+            # Display Outside Temperature and Forecast
+            elif index == 2:
+                magtag.peripherals.neopixel_disable = False
+                magtag.peripherals.neopixels.fill((0, 255, 0))
+                magtag.set_text("")
+
+            # Display Random Saying
+            elif index == 3:
+                magtag.peripherals.neopixel_disable = False
+                magtag.peripherals.neopixels.fill((0, 0, 255))
+                magtag.set_text("")
+
             break
     else:
-        # magtag.peripherals.neopixel_disable = True
-        # magtag.peripherals.neopixels.fill((0, 0, 0))
-        ...
+        magtag.peripherals.neopixel_disable = True
+        magtag.peripherals.neopixels.fill((0, 0, 0))
 
     time.sleep(0.01)
